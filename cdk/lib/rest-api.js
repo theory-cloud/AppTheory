f6fa0c5aaf499d4bdc6d285b8350b92b0704301b
Aron Price <aron23@gmail.com>
2026-01-20 08:23:52 -0500

    Release: v0.2.0-rc.1

--- cdk/lib/rest-api.js
+++ cdk/lib/rest-api.js
@@ -32,7 +32,7 @@ class AppTheoryRestApi extends constructs_1.Construct {
 }
 exports.AppTheoryRestApi = AppTheoryRestApi;
 _a = JSII_RTTI_SYMBOL_1;
-AppTheoryRestApi[_a] = { fqn: "@theory-cloud/apptheory-cdk.AppTheoryRestApi", version: "0.1.0" };
+AppTheoryRestApi[_a] = { fqn: "@theory-cloud/apptheory-cdk.AppTheoryRestApi", version: "0.2.0-rc.1" };
 function resourceForPath(api, inputPath) {
     let current = api.root;
     const trimmed = String(inputPath ?? "")
