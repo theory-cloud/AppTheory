807c2e7baa24a65959c7119ab5b75c0b61722ec3
Aron Price <aron23@gmail.com>
2026-04-29 23:04:19 -0400

    fix(release): recover stable generated artifacts

--- cdk/lib/mcp-server.js
+++ cdk/lib/mcp-server.js
@@ -166,7 +166,7 @@ class AppTheoryMcpServer extends constructs_1.Construct {
 }
 exports.AppTheoryMcpServer = AppTheoryMcpServer;
 _a = JSII_RTTI_SYMBOL_1;
-AppTheoryMcpServer[_a] = { fqn: "@theory-cloud/apptheory-cdk.AppTheoryMcpServer", version: "1.2.0-rc.1" };
+AppTheoryMcpServer[_a] = { fqn: "@theory-cloud/apptheory-cdk.AppTheoryMcpServer", version: "1.2.1" };
 /**
  * Convert a domain name to a Route53 record name relative to the zone.
  */
