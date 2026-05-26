bb03e46e01656ad13b2a01b47b28038a72c9bc75
Aron Price <aron23@gmail.com>
2026-05-26 00:23:42 -0400

    feat(cdk): add AppTheoryKinesisStream (#614)
    Add the AppTheoryKinesisStream CDK construct for Kinesis Data Streams.
    
    Refs THE-1613.
    Refs theory-cloud/AppTheory#571.

---
+++ cdk-go/apptheorycdk/AppTheoryKinesisStream__checks.go
@@ -0,0 +1,60 @@
+//go:build !no_runtime_type_checking
+
+package apptheorycdk
+
+import (
+	"fmt"
+
+	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	"github.com/aws/constructs-go/constructs/v10"
+)
+
+func (a *jsiiProxy_AppTheoryKinesisStream) validateGrantReadParameters(grantee awsiam.IGrantable) error {
+	if grantee == nil {
+		return fmt.Errorf("parameter grantee is required, but nil was provided")
+	}
+
+	return nil
+}
+
+func (a *jsiiProxy_AppTheoryKinesisStream) validateGrantReadWriteParameters(grantee awsiam.IGrantable) error {
+	if grantee == nil {
+		return fmt.Errorf("parameter grantee is required, but nil was provided")
+	}
+
+	return nil
+}
+
+func (a *jsiiProxy_AppTheoryKinesisStream) validateGrantWriteParameters(grantee awsiam.IGrantable) error {
+	if grantee == nil {
+		return fmt.Errorf("parameter grantee is required, but nil was provided")
+	}
+
+	return nil
+}
+
+func validateAppTheoryKinesisStream_IsConstructParameters(x interface{}) error {
+	if x == nil {
+		return fmt.Errorf("parameter x is required, but nil was provided")
+	}
+
+	return nil
+}
+
+func validateNewAppTheoryKinesisStreamParameters(scope constructs.Construct, id *string, props *AppTheoryKinesisStreamProps) error {
+	if scope == nil {
+		return fmt.Errorf("parameter scope is required, but nil was provided")
+	}
+
+	if id == nil {
+		return fmt.Errorf("parameter id is required, but nil was provided")
+	}
+
+	if err := _jsii_.ValidateStruct(props, func() string { return "parameter props" }); err != nil {
+		return err
+	}
+
+	return nil
+}
